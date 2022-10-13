a1330aa89582e7ccd385c1998c9da1158d3b7255
51Sirius <71227522+51Sirius@users.noreply.github.com>
2022-10-13 10:40:32 +0300

    some correct

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-"""This function return area from triangle"""
-
-
 def area_of_triangle(a, h):
     if a <= 0 or h <= 0:
         raise ValueError("Wrong value")
+    """Checks for value error"""
     return 0.5 * a * h
 
 
-n, m = map(int, input("Введите числа для подсчета площади: ").split())
-print(area_of_triangle(n, m))
+"""Return value-area"""
+
+"""This function return area from triangle"""
+
+"""Input numbers from console"""
+"""Print area of our triangle"""
--- test.py
+++ test.py
@@ -8,5 +8,5 @@ class TestArea(unittest.TestCase):
         self.assertEqual(area_of_triangle(10, 2), 10)
         self.assertEqual(area_of_triangle(5, 2), 5)
         self.assertEqual(area_of_triangle(10, 4), 20)
-        self.assertEqual(area_of_triangle(10, 1), 5)
+        self.assertEqual(area_of_triangle(10, 1), 6)
 
